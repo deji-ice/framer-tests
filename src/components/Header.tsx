6101071524d80a7b0608dadffa1fdf30d949d216
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-31 15:01:06 +0100

    refactor: Update Header component with forwardRef for better performance

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,20 @@
-const Header = () => {
+import { forwardRef, Ref } from "react";
+
+const Header = forwardRef((_, ref: Ref<HTMLDivElement>) => {
   return (
     <div className="fixed w-full flex justify-end cursor-pointer mix-blend-difference z-10 p-2.5 box-border">
       <div
         className="relative flex gap-2 flex-col p-8 pointer-events-none before:block before:w-8 before:h-0.5
       before:mix-blend-difference before:bg-white after:block after:w-8 after:h-0.5 after:mix-blend-difference after:bg-white
       "
-      ></div>
+      >
+        <div
+          className="absolute left-0 top-0 w-full h-full pointer-events-auto hover:scale-150 transform transition duration-300 "
+          ref={ref}
+        ></div>
+      </div>
     </div>
   );
-};
+});
 
 export default Header;
