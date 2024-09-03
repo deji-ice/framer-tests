c333570f2e5b290dea7a74255dd34f1f939dc2d1
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-09-03 12:07:27 +0100

    refactor: Add Trailing component for interactive trailing effect

---
+++ src/components/Trailing.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+
+const Trailing = () => {
+  const [windowsWidth, setWindowsWidth] = useState(0);
+
+  useEffect(() => {
+    setWindowsWidth(window.innerWidth);
+  }, []);
+
+  const getBlocks = () => {
+    const blockSize = windowsWidth * 0.05;
+    const noOfBlocks = Math.ceil(window.innerHeight / blockSize);
+    console.log(noOfBlocks, blockSize);
+    return [...Array(noOfBlocks).keys()].map((_, index) => {
+      return (
+        <div
+          onMouseEnter={(e) => colorize(e)}
+          className="w-full h-[5vw] "
+          key={index}
+        ></div>
+      );
+    });
+  };
+
+  const colorize = (e) => {
+    e.target.style.background = "black";
+    setTimeout(() => {
+      e.target.style.background = "transparent";
+    }, 300);
+  };
+  return (
+    <div className="flex h-screen items-center bg-white justify-center">
+      <div className="text-center pointer-events-none uppercase mix-blend-difference text-white z-10 relative font-bold text-[6vw] w-[70%]">
+        <p>We specialize in turning space into complexe shapes</p>
+      </div>
+      <div className={`flex w-full h-full overflow-hidden absolute`}>
+        {windowsWidth > 0 &&
+          [...Array(20).keys()].map((_, index) => {
+            return (
+              <div key={"b_" + index} className={`w-[5vw]`}>
+                {getBlocks()}
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default Trailing;
