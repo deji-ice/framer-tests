86aee99ccb0eaedbf1cfb13219e4dc32ab157ba2
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-09-17 12:51:49 +0100

    Refactor Curve component to use framer-motion for animated path

--- src/components/Curve.tsx
+++ src/components/Curve.tsx
@@ -1,9 +1,45 @@
-
+import { motion } from "framer-motion";
 
 const Curve = () => {
-  return (
-    <div>Curve</div>
-  )
-}
+  const initialPath = `M100 0 L100 ${window.innerHeight} Q-100 ${
+    window.innerHeight / 2
+  } 100 0`;
+  const targetPath = `M100 0 L100 ${window.innerHeight} Q100 ${
+    window.innerHeight / 2
+  } 100 0`;
 
-export default Curve
+  const curve = {
+    initial: {
+      d: initialPath,
+    },
+
+    enter: {
+      d: targetPath,
+
+      transition: { duration: 1, ease: [0.76, 0, 0.24, 1] },
+    },
+
+    exit: {
+      d: initialPath,
+
+      transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+    },
+  };
+
+  return (
+    <svg
+      className={
+        "absolute top-0 h-full w-[100px] left-[-99px] stroke-none fill-[rgb(41,41,41)]"
+      }
+    >
+      <motion.path
+        variants={curve}
+        initial="initial"
+        animate="enter"
+        exit="exit"
+      ></motion.path>
+    </svg>
+  );
+};
+
+export default Curve;
