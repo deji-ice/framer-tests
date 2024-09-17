11461f1b801a2fab8aa9e14208d44fb4885c50bd
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-09-17 12:51:55 +0100

    Refactor Header component to update animation styles and remove unused code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,26 +11,24 @@ const Header = forwardRef((props, ref: Ref<HTMLDivElement>) => {
     console.log("isActive", isActive);
   };
   return (
-    <div className="fixed w-[80px] rounded-full flex justify-center items-center cursor-pointer mix-blend-difference z-[200] right-0 h-[80px] m-5">
+    <div className="fixed w-[80px] rounded-full flex justify-center items-center cursor-pointer mix-blend-difference z-[190] right-0 h-[80px] m-5">
       <Magnetic>
         <div
           onClick={active}
-          className={`after:relative w-full m-auto before:transition-transform before:duration-300 after:transition-transform after:duration-300 gap-2 
+          className={`text-red-800 relative after:relative w-full m-auto before:transition-transform before:duration-300 after:transition-transform after:duration-300 gap-2 
             p-8 pointer-events-none before:relative before:top-[5px] before:block before:w-8 before:h-0.5
     before:mix-blend-difference before:bg-white after:block after:w-8 after:h-0.5 
     after:mix-blend-difference after:bg-white after:top-[-5px] 
     ${
       isActive
-        ? "before:top-0 before:-rotate-45 after:top-[-1px] after:rotate-45"
+        ? "before:top-0 z-[200] before:-rotate-45 after:top-[-1px] after:rotate-45"
         : ""
     }`}
         >
           <div className="bounds" ref={ref}></div>
         </div>
       </Magnetic>
-      <AnimatePresence mode={"wait"}>
-        {isActive && <Nav/>}
-      </AnimatePresence>
+      <AnimatePresence mode={"wait"}>{isActive && <Nav />}</AnimatePresence>
     </div>
   );
 });
