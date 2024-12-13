fc67aa9b47a3e83a72000a2639a2389edd13ed7b
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-12-13 12:25:16 +0100

    Render project entries using ScrollCard component in Projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,14 @@
+import { projects } from "../assets/mockData";
+import ScrollCard from "../components/ScrollCard";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="mt-[50vh] mb-[50vh] h-full">
+      {projects.map((project, i) => {
+        return <ScrollCard key={`p_${i}`} {...project} i={i} />;
+      })}
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
