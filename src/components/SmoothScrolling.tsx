217fb4090c446071d3e4ff8eb810741400ffe678
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-04-05 17:12:34 +0100

    Add SmoothScrolling component and integrate Lenis for smooth scrolling effects

---
+++ src/components/SmoothScrolling.tsx
@@ -0,0 +1,11 @@
+import { ReactLenis } from "lenis/react";
+
+function SmoothScrolling({ children }: { children: React.ReactNode }) {
+  return (
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+      {children}
+    </ReactLenis>
+  );
+}
+
+export default SmoothScrolling;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import SmoothScrolling from "./components/SmoothScrolling.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+    <SmoothScrolling>
       <App />
+      </SmoothScrolling>
     </BrowserRouter>
   </StrictMode>
 );
